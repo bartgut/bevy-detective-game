9a8ab99463a55d584d0bb452603fd6523238ee7c
Chocholo <bartosz.gutowski@mail.com>
2024-06-02 19:24:35 +0200

    Rain on train station

--- src/game/items/train_station_rain.rs
+++ src/game/items/train_station_rain.rs
@@ -1,4 +1,5 @@
 use bevy::ecs::system::EntityCommands;
+use bevy::math::vec3;
 use bevy::prelude::*;
 use bevy_hanabi::prelude::*;
 use bevy_hanabi::EffectAsset;
@@ -15,42 +16,41 @@ pub fn train_station_rain_prepare(
     mut items: ResMut<ItemResource>,
     mut effects: ResMut<Assets<EffectAsset>>,
 ) {
-    let mut gradient = Gradient::new();
-    gradient.add_key(0.0, Vec4::new(1., 1., 1., 1.));
-    gradient.add_key(1.0, Vec4::splat(0.));
+    let mut raindrop_gradient = Gradient::new();
+    raindrop_gradient.add_key(0.0, Vec3::splat(1.).extend(0.0));
+    raindrop_gradient.add_key(0.1, Vec3::splat(1.).extend(0.5));
+    raindrop_gradient.add_key(0.9, Vec3::splat(1.).extend(0.5));
+    raindrop_gradient.add_key(1.0, Vec3::splat(1.).extend(0.0));
 
-    // Create a new expression module
-    let mut module = Module::default();
+    let writer = ExprWriter::new();
 
     let size = SetSizeModifier {
-        size: CpuValue::Single(Vec2::new(1.0, 5.0)),
+        size: CpuValue::Uniform((Vec2::new(1.0, 4.0), Vec2::new(1.0, 5.0))),
         ..default()
     };
 
-    let init_pos = SetPositionSphereModifier {
-        center: module.lit(Vec3::ZERO),
-        radius: module.lit(0.5),
-        dimension: ShapeDimension::Surface,
-    };
+    let init_pos =
+        SetAttributeModifier::new(Attribute::POSITION,
+                                  writer.rand(ValueType::Vector(VectorType::VEC3F))
+                                      .mul(writer.lit(Vec3::new(2600., 1., 0.0)))
+                                      .sub(writer.lit(Vec3::new(1300., 0., 0.0)))
+                                      .add(writer.lit(Vec3::new(0.0, 0.0, 0.0)))
+                                      .expr());
 
-    let init_vel = SetVelocitySphereModifier {
-        center: module.lit(Vec3::ZERO),
-        speed: module.lit(500.),
-    };
+    let init_vel =
+        SetAttributeModifier::new(Attribute::VELOCITY, writer.lit(Vec3::new(0., -500., 0.)).expr());
 
-    let lifetime = module.lit(4.);
+    let lifetime = writer.rand(ValueType::Scalar(ScalarType::Float)).mul(writer.lit(0.8)).expr();
     let init_lifetime = SetAttributeModifier::new(Attribute::LIFETIME, lifetime);
 
-    let accel = module.lit(Vec3::new(0., -500., 0.));
-    let update_accel = AccelModifier::new(accel);
+    let module = writer.finish();
 
     let effect = EffectAsset::new(32768, Spawner::rate(200_f32.into()), module)
         .with_name("TrainStationRain")
         .init(init_pos)
         .init(init_vel)
         .init(init_lifetime)
-        .update(update_accel)
-        .render(ColorOverLifetimeModifier { gradient })
+        .render(ColorOverLifetimeModifier { gradient: raindrop_gradient })
         .render(size);
 
     let effect_handle = effects.add(effect);
@@ -69,7 +69,7 @@ impl SpawnableChild for TrainStationRain {
         level.with_children(|parent| {
             parent.spawn(ParticleEffectBundle {
                 effect: ParticleEffect::new(self.effect_description.clone_weak()),
-                transform: Transform::from_xyz(0.0, 460.0, 100.0),
+                transform: Transform::from_xyz(0.0, 224.0, 100.0),
                 ..Default::default()
             });
         });
